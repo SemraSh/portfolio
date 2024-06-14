f3c865b722287ca5461cddc856d9c4d625a9bc20
Semra Shukrieva <semra.shukrieva@gmail.com>
2024-06-14 18:39:04 +0200

    update icons typographies and page content

--- src/app/page.js
+++ src/app/page.js
@@ -2,33 +2,43 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import github_logo from "../../public/github-mark.png";
 import imza from "../../public/imza.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.brand}>
-        <Image
-          style={{ height: "100%", width: "auto" }}
-          src={imza}
-          alt="signature"
-        />
-        <p>
-          this is a place for me to play with the new things I explore as a
-          front end dev
-        </p>
+      {/* ME */}
+      <div className={styles.head}>
+        <text className={styles.name}>semra dora &lpar; shukrieva &rpar;</text>
+        <br />
+        web developer
+      </div>
+      {/* WORKS */}
+      <div className={styles}>
+        <p>Portfolio</p>
       </div>
 
-      <div className={styles.history}>
-        <b>here is a little history of this website</b>
+      {/* JOURNAL */}
+      <div className={styles.journal}>
+        <b>Journaling while bilding this website</b>
         <p>
           <b>10th of June 2024</b> I just started building this website with
           Next.js
         </p>
+        <p>
+          <b>14th of June 2024</b> I struggled to select a font a favicon images
+          and all the UI/UX tools, but getting there
+        </p>
       </div>
-      <div>
+
+      {/* FOOTER */}
+      <div className={styles.footer}>
         <a href="https://github.com/SemraSh" target="_blank">
           <Image src={github_logo} alt="github link" width={23} height={23} />
         </a>
+        <Link href="/">
+          <Image src={imza} alt="signature" />
+        </Link>
       </div>
     </main>
   );
