0c0a4876ba0acce8bec8344720ce62778fb9e082
Semra Shukrieva <semra.shukrieva@gmail.com>
2024-06-26 11:42:25 +0200

    fix menu

--- src/app/components/menu.js
+++ src/app/components/menu.js
@@ -4,8 +4,7 @@ import styles from "./menu.module.css";
 export default function Menu() {
   return (
     <menu className={styles.menu}>
-      <Link href={"/"}>home</Link> |
-      <Link href={"/resume"}>work experience</Link>
+      <Link href={"/"}>home</Link> | <Link href={"/resume"}>work</Link>
     </menu>
   );
 }
